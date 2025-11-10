106e96a57ab2135d40dd91c45218e525133161e1
kimminjung <ekfkawnl644@gmail.com>
2025-11-10 23:37:41 +0900

    fix:role_helper임시 접근 금지

--- grpc_service/helpers/Role_helper.rb
+++ grpc_service/helpers/Role_helper.rb
@@ -6,7 +6,7 @@ require 'role/service_services_pb'
 module RoleHelper
   # 역할별 권한 레벨 테이블
     ROLE_LEVELS = {
-      
+
     "student"   => 1,
     "keeper"    => 2,
     "leader"    => 3,
@@ -26,17 +26,19 @@ module RoleHelper
     ) if user_code.blank?
 
     user = ::User.find_by(user_number: user_code)
-    raise GRPC::BadStatus.new_status_exception(
-      GRPC::Core::StatusCodes::NOT_FOUND,
-      "해당 사용자를 찾을 수 없습니다."
-    ) if user.nil?
 
+    # raise GRPC::BadStatus.new_status_exception(
+    #   GRPC::Core::StatusCodes::NOT_FOUND,
+    #   "해당 사용자를 찾을 수 없습니다."
+    # ) if user.nil?
+  # DB에 없어도 임시로 user_id = 0 으로 통과시킴
+    user_id = user ? user.id : 0
     [user.id, user_role]
   end
 
-  # 권한 확인 (임시)
-  def self.has_authority?(user_id, required_level)
-    puts "임시로 권한 확인 통과 (Kafka 미구현 상태)"
-    true
-  end
+  # # 권한 확인 (임시)
+  # def self.has_authority?(user_id, required_level)
+  #   puts "임시로 권한 확인 통과 (Kafka 미구현 상태)"
+  #   true
+  # end
 end
